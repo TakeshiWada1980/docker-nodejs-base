049fe9e6a5ef5df84708fefefa73e55b34df0c30
T.Wada <28886385+TakeshiWada1980@users.noreply.github.com>
2022-03-02 17:43:44 +0900

    WebSocketの機能を追加

--- work/src/app.ts
+++ work/src/app.ts
@@ -1,9 +1,30 @@
-import http from 'http';
+import express from 'express';
+import ws from 'ws';
+import fs from 'fs';
 
-const port = 3000;
-const server = http.createServer((req, res) => {
-  res.end('Hello World2...')
-}).listen(port);
+const port_http = 3000;
+const port_ws = 3001;
 
-console.log(`Server running on port ${port}`);
-console.log(` >> http://localhost:${port} <<`);
+const app = express();
+
+const wss = new ws.Server({ port: port_ws });
+wss.on('connection', ws => {
+  ws.on('message', msg => {
+    console.log(`received: ${msg}`);
+    ws.send('something');
+  });
+  ws.send('something');
+});
+
+app.get('/', (req, res) => {
+  fs.readFile("dist/index.html", (err, data) => {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.write(data);
+    res.end();
+  });
+});
+
+app.listen(port_http, () => {
+  console.log(`Server running on port ${port_http}`);
+  console.log(` >> http://localhost:${port_http} <<`);
+});
